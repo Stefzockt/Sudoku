8ac100711971f3208b69604233b94e405d19dce4
Stefzockt <moser.stefan02@gmail.com>
2022-01-18 18:00:04 +0100

    Added Logarithem to create the Field

--- main.c
+++ main.c
@@ -2,58 +2,55 @@
 #include <math.h>
 #include <ctype.h>
 
+int gameField[9][9];
 #define LENGTH(array) (sizeof(array)/sizeof(array[0]))
 
 // 0 equals false
 // 1 equals true
 
+void setGameField() {
+    for (int x =0;x <= 9; x++) {
+        for(int y =0; y<=9; y++){
+            gameField[x][y] = 0;
+        }
+    }
+}
+
+
 void Draw(char playerName[]){
-    int contentField[9][9];
-    int displayedField[15][15];
-    int i = 0;
-    printf("Your name: %20s\n",playerName);
+    printf("Player: %s\n", playerName);
 
-    for (int x = 0; x < 15; x++) {
-        for (int y = 0; y < 15; y++) {
-            if (x % (3 + 1) == 1 && y % (3 + 1) == 1) {
-                displayedField[x][y] = ' ';
+    printf("   ABC DEF GHI\n");
+    for(int i = 0; i < 9; i++){
+        if(i%3==0){
+            printf("   --- --- ---\n");
+        }
+
+        printf("%d ", i+1); // line numbers
+        for(int j = 0; j < 9; j++){
+            int value = gameField[i][j];
+            if(j%3==0){
+                printf("|");
+            }
+            if(value == 0){
+                printf("*");
             } else {
-                if (x % (3 + 1) == 1 && y != 0) {
-                    displayedField[x][y] = '-';
-                } else {
-                    if (y % (3 + 1) == 1 && x != 0) {
-                        displayedField[x][y] = '|';
-                    } else {
-                        // insert fieldValues
-                        if (x != 0 && y != 0) {
-                            displayedField[x][y] = contentField[(x - 2 - ((x - 2) / (3 + 1))) * 3 * 3 +
-                                                                (y - 2 - ((y - 2) / (3 + 1)))];
-                        } else {
-                            displayedField[x][y] = ' ';
-                        }
-
-                    }
-                }
+                printf("%d", value);
             }
         }
+        printf("|\n");
     }
-
-    for (int x = 0; x < 15; x++){
-        for (int y = 0; y < 15; y++) {
-            printf("%c", displayedField[x][y]);
-        }
-        printf(" \n");
-    }
-    return;
+    printf("   --- --- ---\n");
 }
 
 int main() {
     int gameFinished = 0;
-    char playerName[20];
-    printf("Please enter your name:\n");
+    char playerName[15]={};
+    setGameField();
+    printf("Please enter your name:");
     scanf("%20s", playerName);
 
-    //This will go one as long as the game goes
+    //This will go one as long as the game is not finished
     while (0 == gameFinished){
         Draw(playerName);
         gameFinished = 1;
